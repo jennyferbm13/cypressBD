ede11a7ad3c5d07c723cfa235ab3ab0fb106f4e2
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2023-03-29 21:28:28 -0500

    Validando status de las peticiones

--- cypress/e2e/status.cy.js
+++ cypress/e2e/status.cy.js
@@ -10,4 +10,35 @@ describe("Probando  status", () => {
       .its("status")
       .should("eq", 404); //esatdo de falla porque ese lo borramos
   });
+
+  it("Debe de validar el status code fallido y mensaje de error", () => {
+    cy.request({
+      url: "https://pokeapi.co/api/v2/4545",
+      failOnStatusCode: false,
+    }).then((responde) => {
+      expect(responde.status).to.be.eq(404);
+      expect(responde.body).to.be.eq("Not Found");
+    });
+  });
+
+  it("Debe de validar el status code fallido y mensaje de error con array", () => {
+    cy.request({
+      url: "https://rickandmortyapi.com/api/location/3434343",
+      failOnStatusCode: false,
+    }).then((responde) => {
+      expect(responde.status).to.be.eq(404);
+      expect(responde.body.error).to.be.eq("Location not found");
+    });
+  });
+
+  //otra forma
+  it("Debe de validar el status code fallido y mensaje de error con array 2", () => {
+    cy.request({
+      url: "https://rickandmortyapi.com/api/location/3434343",
+      failOnStatusCode: false,
+    }).then((responde) => {
+      expect(responde.status).to.be.eq(404);
+      expect(responde.body).to.have.property("error", "Location not found");
+    });
+  });
 });
