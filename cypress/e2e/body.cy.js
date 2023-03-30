3e62d69a41cf8fcfd58bede0c2e53a681b752961
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2023-03-29 21:19:06 -0500

    Validando el body

---
+++ cypress/e2e/body.cy.js
@@ -0,0 +1,16 @@
+describe("Probando  body", () => {
+  it("Debe de validar el body", () => {
+    cy.request("employees/1")
+      .its("body")
+      .its("first_name")
+      .should("be.equal", "Javier");
+
+    cy.request("employees/1").then((response) => {
+      expect(response.status).to.be.equal(200);
+      expect(response.headers["content-type"]).to.be.equal(
+        "application/json; charset=utf-8"
+      );
+      expect(response.body.first_name).to.be.equal("Javier");
+    });
+  });
+});
