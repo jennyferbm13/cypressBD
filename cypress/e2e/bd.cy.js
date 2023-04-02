5d3c218041469d3381c9bdf0cca35e4e2f8efa24
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2023-04-02 18:43:44 -0500

    probando CRUD en bd mysql

--- cypress/e2e/bd.cy.js
+++ cypress/e2e/bd.cy.js
@@ -4,4 +4,43 @@ describe("Probando headers", () => {
       cy.log(result);
     });
   });
+  it("Insert", function () {
+    cy.task(
+      "queryDB",
+      "INSERT INTO pruebas(nombre,appellido) VALUES('Jennyfer','Belalcazar')"
+    ).then((result) => {
+      cy.log(result);
+      expect(result.affectRows).to.eq(1);
+      cy.wrap(result.insertId).and("id");
+    });
+  });
+  it("Select uno", function () {
+    cy.task("queryDB", `SELECT * FROM pruebas where id=${this.id}`).then(
+      (result) => {
+        cy.log(result);
+        expect(result[0].nombre).to.eq("Jennyfer");
+      }
+    );
+  });
+
+  it("update uno", function () {
+    cy.task(
+      "queryDB",
+      `UPDATE pruebas SET nombre = 'Jenn' where id=${this.id}`
+    ).then((result) => {
+      cy.log(result);
+      expect(result[0].nombre).to.eq("Jennyfer");
+    });
+  });
+
+  it("Borrar", function () {
+    cy.task("queryDB", `DELETE FROM pruebas where id=${this.id}`).then(
+      (result) => {
+        cy.log(result);
+        //importante afectar que afecto uno
+        expect(result.affectRows).to.eq(1);
+        expect(result.serverStatus).to.eq(2); //exitoso
+      }
+    );
+  });
 });
